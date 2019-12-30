a28b30a83e9de17092857ccaa5b149314477e2ad
Daniyar <dds861@gmail.com>
2019-12-30 12:36:37 +0100

    changed adapter

--- app/src/main/java/com/dd/coroutineretrofit/user/AdapterUser.kt
+++ app/src/main/java/com/dd/coroutineretrofit/user/AdapterUser.kt
@@ -3,10 +3,10 @@ package com.dd.coroutineretrofit.user
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dd.coroutineretrofit.R
 import com.dd.coroutineretrofit.model.User
+import kotlinx.android.synthetic.main.content_item.view.*
 
 class AdapterUser(private val list: List<User>) : RecyclerView.Adapter<AdapterUser.ViewHolder>() {
 
@@ -16,16 +16,13 @@ class AdapterUser(private val list: List<User>) : RecyclerView.Adapter<AdapterUs
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.hobby.text = list[position].hobby
-        holder.name.text = list[position].name
+        holder.itemView.tv_hobby.text = list[position].hobby
+        holder.itemView.tv_name.text = list[position].name
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var hobby: TextView = itemView.findViewById<View>(R.id.tv_hobby) as TextView
-        var name: TextView = itemView.findViewById<View>(R.id.tv_name) as TextView
-    }
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
