97b0fb5f4257d23f0eb5876b64e4eb08959e8aa6
Daniyar <dds861@gmail.com>
2019-12-30 10:54:31 +0100

    added try catch

--- app/src/main/java/com/dd/coroutineretrofit/user/MainActivityViewModel.kt
+++ app/src/main/java/com/dd/coroutineretrofit/user/MainActivityViewModel.kt
@@ -4,16 +4,25 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.dd.coroutineretrofit.repository.AppRepository
 import kotlinx.coroutines.delay
+import java.io.IOException
 
 class MainActivityViewModel(private val eventManager: UIEventManager) : ViewModel() {
     private val repository = AppRepository()
 
     fun loadDataFromWeb() = liveData {
-        eventManager.showProgressBar()
-        delay(1000)
-        val receivedData = repository.getList()
-        eventManager.hideProgressBar()
-        eventManager.showToast("Loaded")
-        emit(receivedData)
+        try {
+            eventManager.showProgressBar()
+            delay(1000)
+            val receivedData = repository.getList()
+            eventManager.hideProgressBar()
+            eventManager.showToast("Loaded")
+            emit(receivedData)
+        } catch (e: IOException) {
+            eventManager.showToast("IOException")
+            eventManager.hideProgressBar()
+        } catch (e: Exception) {
+            eventManager.showToast("Exception")
+            eventManager.hideProgressBar()
+        }
     }
 }
